f9ba410d2d7a34a34346f2fb72da438770b77df6
him <him@aicd.me>
2024-10-25 20:19:27 +0200

    fix for vite

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,27 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  optimizeDeps: {
+    exclude: [
+      'vue-demi',
+      'chunk-EAUNOWDZ',
+      'chunk-YFT6OQ5R'
+    ],
+    force: true,
+    entries: [
+      './src/**/*.vue',
+      './src/**/*.ts'
+    ]
+  },
+  build: {
+    target: 'esnext',
+    minify: 'terser',
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          'vendor': ['vue', 'vue-router', 'pinia']
+        }
+      }
+    }
+  }
 })
